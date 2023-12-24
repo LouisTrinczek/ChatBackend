98f625839a2297e6bf823bb69345842b21d48845
Louis Trinczek <trinczeklouis@gmail.com>
2023-12-24 14:10:50 +0100

    chore(database): Start adding Database Integration

---
+++ ChatBackend/Data/ChatDataContext.cs
@@ -0,0 +1,18 @@
+﻿using ChatBackend.Models;
+
+namespace ChatBackend.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ChatDataContext : DbContext
+{
+    // TODO: Add Configuration file or something
+    private readonly string _connectionString = @"Server=localhost; Port=40001; User ID=root; Password=secret; Database=chat";
+
+    public DbSet<User> Users { get; set; } = null!;
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+    }
+}
---
+++ ChatBackend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChatBackend.Models;
+
+public class User : BaseEntity
+{
+    [Key]
+    [MaxLength(100)]
+    public string Id { get; set; } = null!;
+
+    [Column]
+    [MaxLength(100)]
+    public string Username { get; set; } = null!;
+
+    [Column]
+    [MaxLength(100)]
+    public string Password { get; set; } = null!;
+}
